ae778e4110d5205a6b3372b8d04c9e107e92730b
Aris-NBTN <116520283+Aris-NBTN@users.noreply.github.com>
2024-10-24 20:30:43 +0700

    UD

--- assets/customPageApi-Cy2zlc5M.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./index-D4WFe0GN.js";const o="/v1/custom-page",a=async s=>await e.get(`${o}/masonry/${s}`),r=async s=>await e.post(`${o}/masonry`,s),t=async(s,n)=>await e.put(`${o}/masonry/${s}`,n),c=async s=>await e.delete(`${o}/masonry-slug/${s}`),m={getSingleMasonry:a,addMasonry:r,delMasonry:c,updateMasonry:t};export{m};
